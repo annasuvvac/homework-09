1c1329d2bd2d937f8bf4fffa2e296d2c31e87a7b
Ana Suvac <anasuvac@yahoo.com>
2020-01-02 16:23:28 -0500

    fix errors

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# homework-09
+# Developer-Profile-Generator
+ 09 homework
+
+# Project Description
+This app is run in the terminal using Node. To use the app first type "npm install" and hit "enter" to install all required dependencies.
+
+The Developer Profile Generator uses Inquirer to prompt the user for a GitHub user name and a color in the terminal. The app uses this information to query the GitHub data API and get information on the GitHub user that was entered. The app then uses FS to generate an html file ("profile.html") displaying information about the GitHub user including their profile picture, bio, number of repositories, etc. It also contains links to a google map of the GitHub user's location, to their GitHub profile, and to their blog. The information is displayed on cards that have a background color that matches the color the user entered in the Inquirer prompt (the color must be a recognized color name).
+# Screenshots  
+ <img src="images/screen.develop.png">
+ <img src="images/screen.develop2.png">
+
+
+ # Links :
+
+ https://ana199816.github.io/homework-09/ - WebSite
+
+ https://github.com/Ana199816/homework-09 - GitHub
+
+Technologies and Techniques Used
+This project demonstrates the use of several web development technologies. It uses Node to run the Javascript rather than a web browser. It uses the native FS module for the creation of an HTML file, and also uses third-party node modules including Inquirer to collect and use user inputs, Axios to perform http requests to third-party data APIs, and Electron for the generation of a PDF file. Template literals are used to generate HTML and easily insert javascript variables in the required locations. The GitHub data API response is manipulated in order to capture the desired information about a GitHub user. Additionally, object destructuring is used to neatly capture the information entered by the user via the Inquirer module.
--- index.js
+++ index.js
@@ -1,66 +1,138 @@
+const axios = require("axios");
 const inquirer = require("inquirer");
 const fs = require("fs");
-const axios = require("axios");
-const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
-const pdf = require("html-pdf");
-const questions = [
-  {
-    type: "input",
-    message: "What's your GitHub username?",
-    name: "username"
-  },
-  {
-    type: "list",
-    message: "Which color do you Perfer?",
-    name: "color",
-    choices: ["green", "blue", "pink","red"]
-  }
-]
+
+convertFactory = require('electron-html-to');
+
+const conversion = convertFactory({
+converterPath: convertFactory.converters.PDF
+});
+
 
 inquirer
-  .prompt(questions)
-  .then(function(userInput){ //what's this function about?
+.prompt([{
+    message: "Enter GitHub username",
+    name: "username"
+},
+{
+    message: "What is your favorite color?",
+    name: "color"
+}])
+.then(function({username, color}) {
+    const queryUrl = `https://api.github.com/users/${username}`;
 
-  let userName = userInput.username;
-  userName = userName.toLowerCase().trim();
-  let userColor = userInput.color;
+    axios.get(queryUrl)
+    .then(function(response) {
+        const picUrl = response.data.avatar_url;
+        const location = response.data.location;
+        const gitHubUrl = response.data.html_url;
+        const blog = response.data.blog;
+        const bio = response.data.bio;
+        const numRepos = response.data.public_repos;
+        const numFollowers = response.data.followers;
+        const numFollowing = response.data.following;
+        let numStarred = 0;
 
-  let data = {
-    username: userName,
-    color: userColor
-  }
-    callAxios(data)
-  });
-  
-function callAxios(data){
-  const queryURL = `https://api.github.com/users/${data.username}`;
-  axios.get(queryURL)
-  .then(function(res){
-    console.log(res);
-    res.data.color = data.color;
-    writeToHTML(res.data);  
-  });
-}
+        const modLocation = location.split(" ").join("%20");
 
-const generateHTML= require("./generateHTML");  
+        axios.get(`https://api.github.com/users/${username}/starred`)
+        .then(function(response) {
+            numStarred = response.data.length;
+            const newHtml = `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>${username}'s Resume</title>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            </head>
+            <body>
+                <div class="container">
+                    <div class="row my-5">
+                        <div class="col-12">
+                            <div class="card text-center">
+                                <img src="${picUrl}" class="card-img-top mx-auto rounded-circle border border-warning" alt="Profile picture" style="width: 18rem;">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 36px;">${username}</p>
+                                    <p class="card-text" style="font-size: 24px;">${bio}</p>
+                                    <a class="card-link" href="https://google.com/maps/place/${modLocation}">${location}</a>
+                                    <a class="card-link" href="${gitHubUrl}">GitHub</a>
+                                    <a class="card-link" href="${blog}">Blog</a>
+                                </div>
+                              </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">Public Repositories</p>
+                                    <p class="card-text" style="font-size: 20px;">${numRepos}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">Followers</p>
+                                    <p class="card-text" style="font-size: 20px;">${numFollowers}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">GitHub Stars</p>
+                                    <p class="card-text" style="font-size: 20px;">${numStarred}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">Following</p>
+                                    <p class="card-text" style="font-size: 20px;">${numFollowing}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-function writeToHTML(res){ 
-  let htmlContent = generateHTML(res);
-  writeFileAsync(`${res.name}_profile.html`, htmlContent);
-  console.log("Successfully wrote to html!");
-  printToPdf(htmlContent,res);
-}
+                <style type="text/css">
+                    body {
+                    background-color: white;
+                    -webkit-print-color-adjust: exact !important;
+                    }
 
-function printToPdf(html,res){
-  let options = { format: 'Letter' };
-  pdf.create(html, options).toFile(`${res.name}_profile.pdf`, function (err) {
-    if (err)
-      return console.log(err);
-    console.log("Successfully printed to pdf!");
-  });
-}
+                    .card {
+                        background-color: ${color};
+                        color: white;
+                    }
 
+                    .card-link {
+                        color: white;
+                    }
+                </style>
+            </body>
+            </html>`;
 
+            conversion({ html: newHtml }, function(err, result) {
+                if (err) {
+                    return console.error(err);
+                }
+                
+            
+                result.stream.pipe(fs.createWriteStream('./index.pdf'));
+                conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+            });
+               
 
-
+            fs.writeFile("profile.html",newHtml,function(err) {
+                if (err) throw err;
+            });
+        });
+    });
+});
